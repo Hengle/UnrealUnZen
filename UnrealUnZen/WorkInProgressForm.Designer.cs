3cb26edc0ea21afaf7a00455502b9b3a7c99bec9
DESKTOP-PCQMQK8\Kulagin <sergeant.coolagin@gmail.com>
2023-11-29 12:09:02 +0200

    Update WorkInProgressForm. Add reporting unpacking process.

--- UnrealUnZen/WorkInProgressForm.Designer.cs
+++ UnrealUnZen/WorkInProgressForm.Designer.cs
@@ -28,48 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.CancelButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.ProgressLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // CancelButton
+            // ProgressLabel
             // 
-            this.CancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.CancelButton.Location = new System.Drawing.Point(12, 63);
-            this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Size = new System.Drawing.Size(380, 60);
-            this.CancelButton.TabIndex = 0;
-            this.CancelButton.Text = "Cancel";
-            this.CancelButton.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(347, 51);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Work in progress";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ProgressLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProgressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ProgressLabel.Location = new System.Drawing.Point(0, 0);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(404, 263);
+            this.ProgressLabel.TabIndex = 1;
+            this.ProgressLabel.Text = "Unpacked 300 out of 300000 files";
+            this.ProgressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // WorkInProgressForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 134);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.CancelButton);
+            this.ClientSize = new System.Drawing.Size(404, 263);
+            this.Controls.Add(this.ProgressLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "WorkInProgressForm";
-            this.Text = "WorkInProgressForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Work In Progress Form";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button CancelButton;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ProgressLabel;
     }
 }
--- UnrealUnZen/WorkInProgressForm.cs
+++ UnrealUnZen/WorkInProgressForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UEcastocLib;
 
 namespace UnrealUnZen
 {
@@ -15,6 +16,30 @@ namespace UnrealUnZen
         public WorkInProgressForm()
         {
             InitializeComponent();
+        }
+
+        public void OnFileUnpacked(UCasDataParser.FileUnpackedEventArguments fileUnpackedEventArguments)
+        {
+            Invoke((MethodInvoker)delegate
+            {
+                ulong Gigabyte = (ulong)Math.Pow(2.0, 30.0);
+                var currentFileNumber = fileUnpackedEventArguments.CurrentFileNumber;
+                var totalFilesNumber = fileUnpackedEventArguments.TotalFilesNumber;
+                var filesUnpackedSize = fileUnpackedEventArguments.FilesUnpackedSize;
+                var allFilesSize = fileUnpackedEventArguments.AllFilesSize;
+                double filesUnpackedSizeGB = (double)filesUnpackedSize / Gigabyte;
+                double allFilesSizeGB = (double)allFilesSize / Gigabyte;
+
+                ProgressLabel.Text = 
+                    $"Unpacked {currentFileNumber} out of {totalFilesNumber} files\n" +
+                    $"{filesUnpackedSizeGB:0.##} / {allFilesSizeGB:0.##} GB";
+            });
+        }
+
+        public void OnUnpackingFinished(int filesUnpacked)
+        {
+            MessageBox.Show(filesUnpacked + " file(s) extracted!");
+            Invoke((MethodInvoker)Close);
         }
     }
 }
